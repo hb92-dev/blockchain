92222004d95299bdbf1d8a7147fb85c1b37670fe
hb92-dev <57457620+hb92-dev@users.noreply.github.com>
2020-03-20 22:51:36 +0100

    Update exceptions, imports, and requests

--- app/exception.py
+++ app/exception.py
@@ -1,4 +1,31 @@
+
 class BlockchainException(Exception):
-    def __init__(self, message, code):
-        Exception.__init__(self, message)
-        self.code = code
+    """
+    Base class for all BlockchainException errors.
+    """
+    pass
+
+class ValueError(BlockchainException):
+    """
+    Raised when does not contain the required data.
+    """
+    pass
+
+class TypeError(BlockchainException):
+    """
+    Raised when does not contain the required type.
+    """
+    pass
+
+class EmptyError(BlockchainException):
+    """
+    Raised when there is nothing to transfer.
+    """
+    pass
+
+class CannotValidation(BlockchainException):
+    """
+    Raised when something does not pass a validation check.
+    """
+    pass
+
