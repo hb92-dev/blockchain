f8bd8240e2f140258004929daf5d95bd9a088303
Eranda12 <43314677+Eranda12@users.noreply.github.com>
2020-03-21 23:11:46 +0100

    Overriding compute_hash method of Block class

--- app/user.py
+++ app/user.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from app.block import Block
 from flask_bcrypt import Bcrypt
+from hashlib import sha256
 from app import app
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-class User():
+
+class User(Block):
+
+    def _init(self):
+        Block.__init__(self, 0, [], 0, '0')
+
     def generatePasswordHash(self, password):
         return bcrypt.generate_password_hash(password)
+
+    def _compute_hash(self):
+        string = "encryptionkey"
+        return sha256(string.encode()).hexdigest()
